85ef58d294b2498a85a1eb38651d233b1838d41e
RodrigoPablo <rpablo30@hotmail.com>
2023-03-05 14:33:34 -0300

    Iniciando o projeto de agendamento de pagamento

---
+++ src/main/java/io/github/rpablo30/Rodrigo/Pablo/RodrigoPabloApplication.java
@@ -0,0 +1,13 @@
+package io.github.rpablo30.Rodrigo.Pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodrigoPabloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodrigoPabloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rpablo30/Rodrigo/Pablo/RodrigoPabloApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rpablo30.Rodrigo.Pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodrigoPabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
